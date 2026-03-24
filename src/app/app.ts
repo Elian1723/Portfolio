65e2aedc92c5591119aa13abd00c00c9fec12398
Elián Barrios <elianbarrios1707@outlook.com>
2026-03-24 11:05:07 -0600

    add about component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
 import { Home } from "./components/home/home";
 import { Footer } from "./components/footer/footer";
+import { About } from './components/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Home, Footer],
+  imports: [RouterOutlet, Navbar, Home, Footer, About],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
