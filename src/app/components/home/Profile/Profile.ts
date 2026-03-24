024dc00147dca628f49c33b0ab31aef7e9bfa69d
Elian1723 <elianbarrios1707@outlook.com>
2026-03-24 08:23:01 -0600

    add profile component and separate hero component greeting to greeting component

---
+++ src/app/components/home/Profile/Profile.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  imports: [],
+  templateUrl: './Profile.html',
+})
+export class Profile { }
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { Icon } from "../../shared/components/icon/icon";
 import { Carousel } from "./carousel/carousel";
 import { Octocat } from "../../shared/components/octocat/octocat";
+import { Greeting } from "./greeting/greeting";
+import { Profile } from "./Profile/Profile";
 
 @Component({
   selector: 'app-home',
-  imports: [Carousel, Octocat],
+  imports: [Carousel, Octocat, Greeting, Profile],
   templateUrl: './home.html',
 })
 export class Home { }
