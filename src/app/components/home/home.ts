236f47bc493a83d64cd55133119ae4b48159e5ed
Elián Barrios <elianbarrios1707@outlook.com>
2026-03-23 21:30:36 -0600

    add octocat component and greeting

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Icon } from "../../shared/components/icon/icon";
 import { Carousel } from "./carousel/carousel";
+import { Octocat } from "../../shared/components/octocat/octocat";
 
 @Component({
   selector: 'app-home',
-  imports: [Carousel],
+  imports: [Carousel, Octocat],
   templateUrl: './home.html',
 })
 export class Home { }
---
+++ src/app/shared/components/octocat/octocat.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-octocat',
+  imports: [],
+  templateUrl: './octocat.html',
+})
+export class Octocat { }
