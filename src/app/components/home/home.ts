de8fbbc7390c6bdff4953e84c01ebbec77bf09b9
Elian1723 <elianbarrios1707@outlook.com>
2026-03-26 07:51:12 -0600

    add anchors for sections and delete octocat component

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { Carousel } from "./carousel/carousel";
-import { Octocat } from "../../shared/components/octocat/octocat";
 import { Greeting } from "./greeting/greeting";
 import { ProfileCircle } from "./profile-circle/profile-circle";
 
 @Component({
   selector: 'app-home',
-  imports: [Carousel, Octocat, Greeting, ProfileCircle],
+  imports: [Carousel, Greeting, ProfileCircle],
   templateUrl: './home.html',
 })
 export class Home { }
--- src/app/shared/components/octocat/octocat.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-octocat',
-  imports: [],
-  templateUrl: './octocat.html',
-})
-export class Octocat { }
