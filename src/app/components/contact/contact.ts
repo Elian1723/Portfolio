ba368dd5941787b7f115fc0623b9379f817d16aa
Elian1723 <elianbarrios1707@outlook.com>
2026-03-26 15:44:29 -0600

    add contact design

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   imports: [],
   templateUrl: './contact.html',
 })
-export class Contact { }
+export class Contact {
+  private modalRef = viewChild<ElementRef<HTMLDialogElement>>('warning_modal');
+
+  protected submitEmail(e: Event): void {
+    e.preventDefault();
+
+    const modal = this.modalRef()?.nativeElement;
+
+    if (modal) {
+      modal.showModal();
+    }
+  }
+}
