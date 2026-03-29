2d8fdb06ab1b01b56e0d4085453124d5bbfb5b98
Elián Barrios <elianbarrios1707@outlook.com>
2026-03-29 01:53:17 -0600

    add ngx-number-ticker component and update about section with experience timeline

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { NgxNumberTickerComponent } from '@omnedia/ngx-number-ticker';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [NgxNumberTickerComponent],
   templateUrl: './about.html',
 })
-export class About { }
+export class About {
+  protected year = signal(new Date().getFullYear());
+}
