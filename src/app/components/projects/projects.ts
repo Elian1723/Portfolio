70b2955c77529923cb293dff64aa84cab57e7ada
Elián Barrios <elianbarrios1707@outlook.com>
2026-03-28 21:48:18 -0600

    add @omnedia/ngx-fade component and update project display with fade animations

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -1,10 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, signal } from '@angular/core';
 import { ProjectService } from '../../services/projectService';
+import { NgxFadeComponent } from '@omnedia/ngx-fade';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [NgxFadeComponent],
   templateUrl: './projects.html',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Projects {
   private projectService = inject(ProjectService);
