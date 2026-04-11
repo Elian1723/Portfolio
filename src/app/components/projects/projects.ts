dae3f36c5835b1fb1dfe8fc6143664bbd14f9352
elianbarrios <elianbarrios1707@outlook.com>
2026-04-11 13:59:37 -0600

    improve responsive design and component accessibility

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -1,17 +1,15 @@
 import { Component, inject, signal } from '@angular/core';
 import { ProjectService } from '../../services/projectService';
 import { NgxFadeComponent } from '@omnedia/ngx-fade';
-import * as socialIcons from '../../shared/icons/socialIcons'
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { IconService } from '../../services/iconService';
+import { ionLogoOctocat } from '@ng-icons/ionicons';
 
 @Component({
   selector: 'app-projects',
   imports: [NgxFadeComponent, NgIcon],
   templateUrl: './projects.html',
-  providers: [provideIcons(socialIcons)]
+  providers: [provideIcons({ionLogoOctocat})]
 })
 export class Projects {
   protected projectService = inject(ProjectService);
-  protected iconService = inject(IconService);
 }
