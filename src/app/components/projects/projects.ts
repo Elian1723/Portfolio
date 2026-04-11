9af0e707bc8b028098bf4e49039071587352879f
elianbarrios <elianbarrios1707@outlook.com>
2026-04-11 14:34:46 -0600

    improve seo and performance

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -2,13 +2,13 @@ import { Component, inject, signal } from '@angular/core';
 import { ProjectService } from '../../services/projectService';
 import { NgxFadeComponent } from '@omnedia/ngx-fade';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { ionLogoOctocat } from '@ng-icons/ionicons';
+import { ionLogoOctocat, ionArrowUpRightBoxOutline } from '@ng-icons/ionicons';
 
 @Component({
   selector: 'app-projects',
   imports: [NgxFadeComponent, NgIcon],
   templateUrl: './projects.html',
-  providers: [provideIcons({ionLogoOctocat})]
+  providers: [provideIcons({ionLogoOctocat}), provideIcons({ionArrowUpRightBoxOutline})]
 })
 export class Projects {
   protected projectService = inject(ProjectService);
